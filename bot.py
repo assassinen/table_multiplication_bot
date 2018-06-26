af68d04cde5dc4d6c7a782ef11ad16aca558996e
ivn <ivn@exante.eu>
2018-06-26 09:04:13 +0300

    make it better

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from configparser import ConfigParser
 # from telegram import ParseMode, Emoji
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram.ext.dispatcher import run_async
-from multiplier import get_input_str, Multiplier
+from multiplier import Multiplier
 
 
 config = ConfigParser()
@@ -17,7 +17,7 @@ config.read_file(open('config.ini'))
 # Create telegram poller with token from settings
 up = Updater(token=config['Telegram']['token'], workers=32)
 dispatcher = up.dispatcher
-multiplier = Multiplier()
+multiplier_dict = {}
 
 
 msg_hello = '''
@@ -39,7 +39,7 @@ msg_command_list = '''
 
 # Welcome message
 def start(bot, update):
-
+    multiplier_dict[update.message.chat_id] = Multiplier()
     msg = msg_hello + msg_command_list
 
     # Send the message
@@ -49,8 +49,10 @@ def start(bot, update):
                          bot_name=bot.name))
 
 def study(bot, update):
-    msg = multiplier.get_next_message()
-    print(update.message.chat_id)
+    if update.message.chat_id not in multiplier_dict:
+        multiplier_dict[update.message.chat_id] = Multiplier()
+    msg = multiplier_dict[update.message.chat_id].get_next_message()
+    # msg = multiplier.get_next_message()
     # Send the message
     bot.send_message(chat_id=update.message.chat_id,
                      text=msg.format(
@@ -58,7 +60,7 @@ def study(bot, update):
                          bot_name=bot.name))
 
 def reset(bot, update):
-    multiplier.reset()
+    multiplier_dict[update.message.chat_id].reset()
 
     msg = msg_reset + msg_continuation + msg_command_list
 
@@ -78,21 +80,23 @@ def setting(bot, update):
                          bot_name=bot.name))
 
 def statistic(bot, update):
-    msg = 'Извините, данная комманда пока не доступна'
+    msg = 'Извините, данная комманда пока не доступна, {bot_id}'
 
     # Send the message
     bot.send_message(chat_id=update.message.chat_id,
                      text=msg.format(
                          user_name=update.message.from_user.first_name,
-                         bot_name=bot.name))
+                         bot_name=bot.name,
+                         bot_id = bot.id))
 
 @run_async
 def process(bot, update):
-    print(multiplier)
+    # print(multiplier)
     # сформировать сообщение ответа
     replay = update.message.text
 
-    msg = multiplier.get_next_message(replay)
+    msg = multiplier_dict[update.message.chat_id].get_next_message(replay)
+    # msg = multiplier.get_next_message(replay)
     if msg.find('ценка') != -1:
         msg = msg + msg_continuation + msg_command_list
     # print(msg, msg.find('ценка'))
