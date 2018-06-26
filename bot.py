9dcc5451c3e6f6d02248ed24580d6c5103640dfa
ivn <ivn@exante.eu>
2018-06-26 23:40:38 +0300

    refactoring

--- bot.py
+++ bot.py
@@ -19,17 +19,18 @@ up = Updater(token=config['Telegram']['token'], workers=32)
 dispatcher = up.dispatcher
 multiplier_dict = {}
 
-
 msg_hello = '''
 Привет {user_name}! 
 Я - {bot_name}.
 Я помогу тебе выучить таблицу умножения. \n 
 Я умею выполнять следующие комманды: \n'''
-
+msg_task = 'Cколько будет {next_task}?\n'
+msg_grade = 'Оценка - {}\n\n'
+msg_true_response = 'Верно\n'
+msg_false_response = 'Не верно\n'
+msg_input_no_int = 'Ответ не является числом.\n'
 msg_continuation = 'Для продолжение используйте слейдующие комманды: \n'
-
 msg_reset = 'Предыдущие результаты онулированы.\n'
-
 msg_command_list = '''
 /study - начать обучение
 /reset - прервать обучение
@@ -60,9 +61,14 @@ def study(bot, update):
                          bot_name=bot.name))
 
 def reset(bot, update):
-    multiplier_dict[update.message.chat_id].reset()
+    chat_id = update.message.chat_id
+    multiplier = get_multiplier_on_chat_id(chat_id)
 
-    msg = msg_reset + msg_continuation + msg_command_list
+    if multiplier is None or not multiplier._is_run:
+        msg = msg_continuation + msg_command_list
+    else:
+        multiplier.reset()
+        msg = msg_reset + msg_continuation + msg_command_list
 
     # Send the message
     bot.send_message(chat_id=update.message.chat_id,
@@ -97,26 +103,32 @@ def process(bot, update):
     multiplier = get_multiplier_on_chat_id(chat_id)
     next_task = ''
 
-    # print(chat_id, replay, multiplier)
-
     if multiplier is None or not multiplier._is_run:
         msg = msg_continuation + msg_command_list
-    elif replay.isdigit():
-        if multiplier.response(replay):
-            msg = 'Верно\nСколько будет {}? '
-        else:
-            msg = 'Не верно\nСколько будет {}? '
-        multiplier.set_input_str()
-        next_task = multiplier.get_input_str()
-
     else:
-        msg = 'Ответ не является числом.\nСколько будет {}? '
+        if replay.isdigit():
+            if multiplier.response(replay):
+                msg = msg_true_response + get_next_message(multiplier)
+            else:
+                msg = msg_false_response + get_next_message(multiplier)
+        else:
+            msg = msg_input_no_int + msg_task
+
         multiplier.set_input_str()
         next_task = multiplier.get_input_str()
 
     # Send the message
     bot.send_message(chat_id=update.message.chat_id,
-                     text=msg.format(next_task))
+                     text=msg.format(next_task=next_task))
+
+
+def get_next_message(multiplier):
+    if sum(multiplier.rez) < multiplier._number_steps:
+        return msg_task
+    else:
+        grade = multiplier.grade()
+        multiplier.reset()
+        return msg_grade.format(grade) + msg_continuation + msg_command_list
 
 
 def get_multiplier_on_chat_id(chat_id=None):
